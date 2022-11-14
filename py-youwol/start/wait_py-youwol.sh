2706665f93b528d8db0af2c177fbf6a06e63fec7
Julien Décharne <90379516+youwol-jdecharne@users.noreply.github.com>
2022-11-14 13:19:09 +0100

    👷  py-youwol/start : wait longer for py-youwol

--- py-youwol/start/wait_py-youwol.sh
+++ py-youwol/start/wait_py-youwol.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
+timeout=30
 jq_cmd='if .status == "py-youwol ok" then halt else error ("incorrect status from py-youwol") end'
 
 # wait for py-youwol readiness
 echo "::group::Trying at most 10 times to call healtz endpoint, waiting 1 seconde between retries"
-for try in $(seq 1 1 10); do
-  echo "try $try/10 to contact py-youwol instance"
+for try in $(seq 1 1 $timeout); do
+  echo "try $try/$timeout to contact py-youwol instance"
   response=$(curl --silent 'http://localhost:2001/healthz' || echo "No response")
   if echo "${response}" | jq -e "${jq_cmd}"; then
     echo "Success : py-youwol is ready"
