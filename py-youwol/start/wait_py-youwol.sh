438b9a5c845ac3f5fcd731efe8ccb86cf130d32d
Julien DÉCHARNE <jdecharne@youwol.com>
2022-11-22 15:43:08 +0100

    👷 py-youwol/start : do not use jq halt command

--- py-youwol/start/wait_py-youwol.sh
+++ py-youwol/start/wait_py-youwol.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 timeout=30
-jq_cmd='if .status == "py-youwol ok" then halt else error ("incorrect status from py-youwol") end'
+jq_cmd='if .status == "py-youwol ok" then "py-youwol started and healthy" else error ("incorrect status from py-youwol") end'
 
 # wait for py-youwol readiness
-echo "::group::Trying at most 10 times to call healtz endpoint, waiting 1 seconde between retries"
+echo "::group::Trying at most $timeout times to call healtz endpoint, waiting 1 second between retries"
 for try in $(seq 1 1 $timeout); do
   echo "try $try/$timeout to contact py-youwol instance"
   response=$(curl --silent 'http://localhost:2001/healthz' || echo "No response")
@@ -20,5 +20,5 @@ for try in $(seq 1 1 $timeout); do
 done
 # If execution flow is here, something went wrong
 echo "::endgroup::"
-echo "::error::Failed to contact py-youwol instance after 10 seconds"
+echo "::error::Failed to contact py-youwol instance after 30 seconds"
 echo "result=failure" >> "$GITHUB_OUTPUT"
